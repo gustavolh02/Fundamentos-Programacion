c6a1c8e16b7b2add22a7395014bba12a9c956a5a
gustavo020802 <48569781+gustavo020802@users.noreply.github.com>
2020-11-04 19:27:13 -0600

    Add files via upload

---
+++ programa3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+
+    int n1;
+    int n2;
+    int suma;
+
+    n1 = 89;
+    n2 = 78;
+    suma = n1 + n2;
+    printf("El resultado de la suma es: %d", suma);
+
+    return 0;
+}
+
