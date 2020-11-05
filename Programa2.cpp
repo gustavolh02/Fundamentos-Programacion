79c33458ce92aa1e0c45279c81dc57265882abe2
gustavo020802 <48569781+gustavo020802@users.noreply.github.com>
2020-11-04 19:26:42 -0600

    Create Programa2.cpp

---
+++ Programa2.cpp
@@ -0,0 +1,8 @@
+#include  <stdio.h>
+int main() {
+	char nombre[20];
+	printf("Hola, me llamo C++, y tu?\n");
+	scanf("%s", &nombre[0]);
+	printf("Me alegro de conocerte %s\n", nombre);
+	return 0;
+}
